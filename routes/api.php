e3a35cf5622ef473b9b80f0165649c6a693f026d
Shorunke999 <134306919+Shorunke999@users.noreply.github.com>
2024-10-17 22:27:27 -0700

    stripe first webhook recieved

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/stripe_webhooks',function(Request $request){
+    Log::info('request body of webhook is',[
+        $request->all()
+    ]);
+});
