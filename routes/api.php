5bcc52ab9dab860c8cb7e9bc409b6ffdfcf83597
Shorunke999 <134306919+Shorunke999@users.noreply.github.com>
2024-10-30 04:46:42 -0700

    the stripe checkout endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,35 +1,64 @@
 <?php
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use Laravel\Cashier\Cashier;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/stripe_webhooks',function(Request $request){
-    Log::info('request body of webhook is',[
-        $request->all()
-    ]);
-});
 
-Route::get('/product/checkout',function(Request $request){
-    $request->validate([
-        'cart' => 'array|required',
-    ]);
+Route::get('/checkout/success', function (Request $request) {
+    $sessionId = $request->get('session_id');
+
+    if ($sessionId === null) {
+        return;
+    }
+
+    $session = Cashier::stripe()->checkout->sessions->retrieve($sessionId);
+
+    if ($session->payment_status !== 'paid') {
+        //return;
+        return view('dashboard')->with('msg','Payment not Successfull');
+    }
+
+    $orderId = $session['metadata']['order_id'] ?? null;
+
+    $order = Order::findOrFail($orderId);
+
+    $order->update(['status' => 'completed']);
+
+    return view('checkout-success', ['order' => $order]);
+})->name('checkout-success');
+
+// Checkout initiation route
+Route::get('/product/checkout', function (Request $request) {
+    $request->validate(['cart' => 'array|required']);
+
     $total_price = 0;
-    foreach($request->cart as $product){
-        $total_price_of_product = $product['number'] * $product['price'];
-        $total_price += $total_price_of_product;
+    foreach ($request->cart as $product) {
+        $total_price += $product['number'] * $product['price'];
     }
-    $user = Auth::user();
-    if ($user && $total_price>0){
-        $request->user()->checkout($total_price * 100,'usd',[
-            'success_url' => url('/checkout/success'),
-            'cancel_url' => url('/checkout/cancel'),
-        ]);
+
+    if ($total_price <= 0 || !$request->user()) {
+        return response()->json(['message' => 'User is not authenticated or total price is zero.'], 400);
     }
-    return response()->json(['message'=>'User is not Authenticated or Total price cant be Zero'],400);
-});
+
+    $order = Order::create([
+        'user_id' => $request->user()->id,
+        'status' => 'pending',
+        'meta_data' => json_encode($request->cart)
+    ]);
+
+    return $request->user()->checkout($total_price * 100, 'usd', [
+        'success_url' => url('/checkout/success?session_id={CHECKOUT_SESSION_ID}'),
+        'cancel_url' => url('/checkout/cancel'),
+        'metadata' => ['order_id' => $order->id],
+    ]);
+})->name('checkout');
+
+
