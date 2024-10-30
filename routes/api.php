1c678c07fa80340d42e029165e27fab10bd23913
Shorunke999 <134306919+Shorunke999@users.noreply.github.com>
2024-10-29 22:57:50 -0700

    the stripe checkout endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
@@ -12,4 +13,23 @@ Route::post('/stripe_webhooks',function(Request $request){
     Log::info('request body of webhook is',[
         $request->all()
     ]);
+});
+
+Route::get('/product/checkout',function(Request $request){
+    $request->validate([
+        'cart' => 'array|required',
+    ]);
+    $total_price = 0;
+    foreach($request->cart as $product){
+        $total_price_of_product = $product['number'] * $product['price'];
+        $total_price += $total_price_of_product;
+    }
+    $user = Auth::user();
+    if ($user && $total_price>0){
+        $request->user()->checkout($total_price * 100,'usd',[
+            'success_url' => url('/checkout/success'),
+            'cancel_url' => url('/checkout/cancel'),
+        ]);
+    }
+    return response()->json(['message'=>'User is not Authenticated or Total price cant be Zero'],400);
 });
